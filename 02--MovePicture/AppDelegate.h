ba16f9e1940ae013638c489b66c72009ecf31828
raineryue <jinlei_yue@sina.com>
2015-10-29 15:49:52 +0800

    Initial Commit

---
+++ 02--MovePicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02--MovePicture
+//
+//  Created by Rainer on 15/10/29.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02--MovePicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02--MovePicture
+//
+//  Created by Rainer on 15/10/29.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
