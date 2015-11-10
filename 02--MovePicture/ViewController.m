89940448f6bc451c1fa7e8d3810b2a721e1df2bc
raineryue <it_jinlei.yue@163.com>
2015-11-10 23:41:58 +0800

    左右旋转
    1.采用transform的形式实现按钮移动，和形变。
    2.添加了左右旋转功能实现。

--- 02--MovePicture/ViewController.m
+++ 02--MovePicture/ViewController.m
@@ -16,15 +16,10 @@ typedef enum {
     TodoButtonTypeLeft,     // 左
     TodoButtonTypeDown,     // 下
     TodoButtonTypeRight,    // 右
-    TodoButtonTypeBig,      // 放大
-    TodoButtonTypeTurnLeft, // 左转
-    TodoButtonTypeMall,     // 缩小
-    TodoButtonTypeTurnRight // 右转
 }TodoButtonType;
 
 /** 定义一个常量用来定义缩放尺寸 */
-#define kSizeZoom 10.0
-#define kMovePadding 10
+#define kMovePadding 20.0
 
 @interface ViewController ()
 
@@ -40,45 +35,58 @@ typedef enum {
 }
 
 /**
- *  按钮事件
+ *  上下左右移动按钮事件
  */
-- (IBAction)todoButtonClickAction:(UIButton *)button {
-    // 1.取出头像按钮的Frame
-    CGRect iconPictureFrame = self.iconPicture.frame;
+- (IBAction)moveWithButton:(UIButton *)button {
+    // 1.定义形变的变量,并初始化
+    CGFloat dx = 0,dy = 0;
     
-    // 2.根据点击按钮的tag判断是什么操作，并作出反应
-    switch (button.tag) {
-        case TodoButtonTypeUp:
-            iconPictureFrame.origin.y -= kMovePadding;
-            break;
-        case TodoButtonTypeLeft:
-            iconPictureFrame.origin.x -= kMovePadding;
-            break;
-        case TodoButtonTypeDown:
-            iconPictureFrame.origin.y += kMovePadding;
-            break;
-        case TodoButtonTypeRight:
-            iconPictureFrame.origin.x += kMovePadding;
-            break;
-        case TodoButtonTypeBig:
-            iconPictureFrame.size.height += kSizeZoom;
-            iconPictureFrame.size.width += kSizeZoom;
-            break;
-        case TodoButtonTypeMall:
-            iconPictureFrame.size.height -= kSizeZoom;
-            iconPictureFrame.size.width -= kSizeZoom;
-            break;
-        case TodoButtonTypeTurnLeft:
-            
-            break;
-        case TodoButtonTypeTurnRight:
-            
-            break;
-        default:
-            break;
+    // 2.判断操作是上下移动还是左右移动
+    if (button.tag == TodoButtonTypeUp || button.tag == TodoButtonTypeDown) {
+        dy = (button.tag == TodoButtonTypeUp) ? -kMovePadding : kMovePadding;
     }
     
-    self.iconPicture.frame = iconPictureFrame;
+    if (button.tag == TodoButtonTypeLeft || button.tag == TodoButtonTypeRight) {
+        dx = (button.tag == TodoButtonTypeRight) ? kMovePadding : -kMovePadding;
+    }
+    
+    // 3.将重新设置的frame重新赋值给头像
+    // 3.1.加上收尾式动画
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDelay:0.1];
+    
+    self.iconPicture.transform = CGAffineTransformTranslate(self.iconPicture.transform, dx, dy);
+    
+    [UIView commitAnimations];
+}
+
+/**
+ *  放大缩小按钮事件
+ */
+- (IBAction)zoomWithButton:(UIButton *)button {
+    // 1.定义缩放比例
+    CGFloat scale = (button.tag) ? 1.2 : 0.8;
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDelay:0.1];
+    
+    self.iconPicture.transform = CGAffineTransformScale(self.iconPicture.transform, scale, scale);
+
+    [UIView commitAnimations];
+}
+
+/**
+ *  左右旋转按钮事件
+ */
+- (IBAction)rotateWithButton:(UIButton *)button {
+    CGFloat angle = (button.tag) ? -M_PI_4 : M_PI_4;
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDelay:0.1];
+    
+    self.iconPicture.transform = CGAffineTransformRotate(self.iconPicture.transform, angle);
+    
+    [UIView commitAnimations];
 }
 
 @end
